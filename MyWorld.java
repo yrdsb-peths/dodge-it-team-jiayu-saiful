6c1bbfd1168b6ca4aba46a311e2cf640093ed734
33de9r <chenjiayu2033@gmail.com>
2025-04-22 14:11:36 -0400

    New penguin hero class created and displayed.

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,8 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Penguin hero = new Penguin();
+        addObject(hero, 100, 100);
     }
 }
