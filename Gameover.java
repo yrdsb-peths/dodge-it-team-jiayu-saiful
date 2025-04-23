41322a2ce5063a23619b17907f5d934a547ed8ef
Stewart Chan <dev.codepet@gmail.com>
2025-04-23 14:03:27 -0400

    fix images

--- Gameover.java
+++ Gameover.java
@@ -8,10 +8,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Gameover extends Actor
 {
-    /**
-     * Act - do whatever the Gameover wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    public Gameover() {
+        setImage("images/skull.png");
+    }
+    
     public void act()
     {
         // Add your action code here.
--- Penguin.java
+++ Penguin.java
@@ -8,10 +8,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Penguin extends Actor
 {
-    /**
-     * Act - do whatever the Penguin wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    public Penguin() {
+        setImage("images/tux.png");
+    }
     
     boolean atTop = true;
     
